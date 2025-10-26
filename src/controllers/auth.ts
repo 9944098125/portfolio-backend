ce57dbbe492ac7307daac4bf9264e68ff35fba47
Akella Srinivas Prasad <srini@Akellas-Mac-mini.local>
2025-10-26 19:28:25 +0530

    users part done

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import Users from "../models/Users";
 import bcryptJs from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { sendRegistrationEmail } from "../helpers/sendMail";
 
 export const register = async (
 	req: Request,
@@ -27,6 +28,7 @@ export const register = async (
 			password: hashedPassword,
 		});
 		await newUser.save();
+		sendRegistrationEmail(email);
 		return res.status(201).json({
 			message: "User Registered Successfully !",
 		});
@@ -66,7 +68,7 @@ export const login = async (
 		}
 		const userWithoutPassword = await Users.findOne({
 			email: user?.email,
-		}).select("password");
+		}).select("-password");
 		const token = jwt.sign(
 			{ userId: user?._id, isAdmin: user?.is_admin },
 			process.env.SECRET_TOKEN!
@@ -75,6 +77,75 @@ export const login = async (
 			message: "Login Success !",
 			user: userWithoutPassword,
 			token: token,
+		});
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const updateUser = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const { userId } = req.params;
+		await Users.findByIdAndUpdate(
+			userId,
+			{ $set: { ...req.body } },
+			{ new: true }
+		);
+		return res.status(200).json({
+			message: "Updated the user successfully",
+		});
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const getAllUsers = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const users = await Users.find();
+		return res.status(200).json({
+			message: "Fetched all the users successfully",
+			data: users,
+		});
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const getUserById = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const { userId } = req.params;
+		const user = await Users.findOne({ _id: userId }).select("-password");
+		return res.status(200).json({
+			message: "Fetched the user successfully",
+			data: user,
+		});
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const deleteUser = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const { userId } = req.params;
+		await Users.findByIdAndDelete(userId);
+		return res.status(200).json({
+			message: "User has been deleted successfully",
 		});
 	} catch (err) {
 		next(err);
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,12 +1,32 @@
 import { Router } from "express";
-import { login, register } from "../controllers/auth";
+import {
+	deleteUser,
+	getAllUsers,
+	getUserById,
+	login,
+	register,
+	updateUser,
+} from "../controllers/auth";
+import { verifyAdmin, verifyOwner } from "../middleware/verify";
 
 const router = Router();
 
 router.route("/auth/login").post(login);
 // route to login
 
-router.route("/auth/register").post(register);
+router.route("/auth/register").post(verifyAdmin, register);
 // route to register
+
+router.route("/auth/update-user/:userId").post(verifyOwner, updateUser);
+// route to update self portfolio fields
+
+router.route("/auth/get-users").get(getAllUsers);
+// to get all the users that is shown in the landing page
+
+router.route("/auth/get-user/:userId").get(verifyOwner, getUserById);
+// to get each user detailed portfolio details
+
+router.route("/auth/delete-user/:userId").delete(verifyOwner, deleteUser);
+// to delete the self portfolio
 
 export default router;
